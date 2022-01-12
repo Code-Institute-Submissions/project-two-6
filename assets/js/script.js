325a503ab94c560428af8a977bc694cbef80f242
ip69719 <ipeters@mail.com>
2022-01-12 12:32:11 +0000

    Create function to start the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,18 @@ let questions = [{
 }];
 
 
-//Get the start button element add event listener to it
+//Get the start button element and add event listener to it
 let startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
+
+/**
+ * Called when start button is clicked, hides the home content of thepade
+ * and takes user to game area
+ */
+function startGame(){
+let homeBlock = document.getElementById("main-container");
+    homeBlock.style.display = 'none';
+    let quizContainer = document.getElementById("quiz-container");
+    quizContainer.style.display = 'block';
+    console.log("test passed"); //remove
+}
