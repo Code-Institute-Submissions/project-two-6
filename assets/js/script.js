ea8009a04148aa978c66c26b55d94269e375d0c9
ip69719 <ipeters@mail.com>
2022-01-12 01:25:56 +0000

    Create questions array for the game

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Create questions for the game
+let questions = [{
+    imgSrc : "assets/images/lexus.png",
+    optionA : "Lexus",
+    optionB : "Lamborghini",
+    optionC : "Lotus",
+    correct : "optionA"
+},{
+    imgSrc : "assets/images/mercedes.png",
+    optionA : "Mazda",
+    optionB : "Mercedes",
+    optionC : "McLaren",
+    correct : "optionB"
+},{
+    imgSrc : "assets/images/toyota.png",
+    optionA : "Tesla",
+    optionB : "Tata",
+    optionC : "Toyota",
+    correct : "optionC"
+}];
